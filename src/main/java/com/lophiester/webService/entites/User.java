294e5b5cb3a82f1f7ce4de0db2dc054b7aecd3a0
Charles Yamamoto <51747636+Lophiester@users.noreply.github.com>
2023-05-14 13:14:27 +0900

    Create User.java

---
+++ src/main/java/com/lophiester/webService/entites/User.java
@@ -0,0 +1,39 @@
+package com.lophiester.webService.entites;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Entity
+@Table(name = "tb_user")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+    private String username;
+    @Email
+    private String email;
+    private String phone;
+    private String password;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof User user)) return false;
+
+        return getId().equals(user.getId());
+    }
+
+    @Override
+    public int hashCode() {
+        return getId().hashCode();
+    }
+}
